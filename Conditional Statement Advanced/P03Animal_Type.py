7d38d56ad9e67261dbd9bdaf72fa0d390a7a51b6
Petar Djupanov <78720915+pdjupanov@users.noreply.github.com>
2023-11-09 21:20:25 +0200

    Add files via upload

---
+++ Conditional Statement Advanced/P03Animal_Type.py
@@ -0,0 +1,18 @@
+
+animal = input()
+
+if animal == 'dog':
+    print('mammal')
+
+elif animal == 'crocodile':
+    print('reptile')
+
+elif animal == 'tortoise':
+    print('reptile')
+
+elif animal == 'snake':
+    print('reptile')
+
+else:
+    print('unknown')
+
