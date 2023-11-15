22fcd7be19128279a8d17285433b605272a204c2
Petar Djupanov <78720915+pdjupanov@users.noreply.github.com>
2023-11-15 16:57:47 +0200

    Update P03NewHouse.py

--- Conditional Statements Advanced Exercise/P03NewHouse.py
+++ Conditional Statements Advanced Exercise/P03NewHouse.py
@@ -1,47 +1,40 @@
-
-flower = input()
-quantity_of_flowers = int(input())
+type_flower = input()
+number_flowers = int(input())
 budget = int(input())
 
-# rose = 5  dahlia = 3.80   tulip = 2.80  narciss = 3    gladiolus = 2.50
-
-price = 0
-discount = 0
-
-if flower == 'Roses':
-    price = 5 * quantity_of_flowers
-    if quantity_of_flowers > 80:
-        discount = 0.10
-
-elif flower == 'Dahlias':
-    price = 3.80 * quantity_of_flowers
-    if quantity_of_flowers > 90:
-        discount = 0.15
-
-elif flower == 'Tulips':
-    price = 2.80 * quantity_of_flowers
-    if quantity_of_flowers > 80:
-        discount = 0.15
-
-# the two nxt if statements discount = increase price
-
-elif flower == 'Narcissus':
-    price = 3 * quantity_of_flowers
-    if quantity_of_flowers < 120:
-        discount = 0.15
-        price = price + discount
-
-elif flower == '"Gladiolus':
-    price = 3 * quantity_of_flowers
-    if quantity_of_flowers < 80:
-        discount = 0.20
-        price = price + discount
+price_per_flower = 0
 
 
-diff = price - budget
+if type_flower == 'Roses':
+    price_per_flower = 5
+    if number_flowers > 80:
+       price_per_flower = price_per_flower * 0.9
 
-price = quantity_of_flowers * (price - discount)
+elif type_flower == 'Dahlias':
+    price_per_flower = 3.80
+    if number_flowers > 90:
+        price_per_flower = price_per_flower * 0.85
 
-print()
+elif type_flower == 'Tulips':
+    price_per_flower = 2.80
+    if number_flowers > 80:
+        price_per_flower = price_per_flower * 0.85
 
+elif type_flower == 'Narcissus':
+    price_per_flower = 3
+    if number_flowers < 120:
+        price_per_flower = price_per_flower * 1.15
 
+elif type_flower == 'Gladiolus':
+    price_per_flower = 2.50
+    if number_flowers < 80:
+        price_per_flower = price_per_flower * 1.20
+
+price = price_per_flower * number_flowers
+diff = abs(budget - price)
+
+if budget >= price:
+    print(f"Hey, you have a great garden with {number_flowers} {type_flower} and {diff:.2f} leva left.")
+
+else:
+    print(f"Not enough money, you need {diff:.2f} leva more.")
