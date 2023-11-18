353c85d06a67e9ce8a940fcb2220f1f6f55c38cd
Petar Djupanov <78720915+pdjupanov@users.noreply.github.com>
2023-11-19 00:16:09 +0200

    Update P08number_sequence.py

--- For Loop/P08number_sequence.py
+++ For Loop/P08number_sequence.py
@@ -1,7 +1,15 @@
-n = int(input())
 
-for number in range(n):
-    input_number = int(input())
-    
+input_number = int(input())
 
-    print()
+max_number = min_number = int(input())
+
+for _ in range(input_number-1):
+    current_number = int(input())
+
+    if current_number > max_number:
+        max_number = current_number
+    if current_number < min_number:
+        min_number = current_number
+
+print(f'Max number: {max_number}')
+print(f'Min number: {min_number}')
